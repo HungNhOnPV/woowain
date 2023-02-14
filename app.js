a6467e0cef0bcd5dd232b26636072fe8232fe3e2
anhhungx9propro@gmail.com <Hung271198>
2023-02-15 04:59:03 +0700

    change link

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
+app.get('/apple-app-site-association', (req, res, next) => {
   return res.status(200).json({
     "applinks": {
     "apps": [],
